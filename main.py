736c2714f5c55a28a89f129e9d03f571448c8221
j4nnu <jan_tervo@hotmail.com>
2022-10-05 20:22:50 +0300

    Toinen versio

--- main.py
+++ main.py
@@ -10,54 +10,68 @@
 
 # Tehdään graafinen hirsipuupeli
 
-
+from pickle import TRUE
+import random
 class hirsipuu:
-    arvattu=[]
-    sana=[]
-    vaara=[]
-    kirjain=""
-    yritysta=0
+    oikein=[] #Listaan kerätään oikein arvatut kirjaimet
+    sana=[] #listaan tallennetaan arvailtava sana
+    arvatut_kirjaimet=[] #listaan tallennetaan arvatut kirjaimet.
+    kirjain="" #Arvattu kirjain
+    yritysta=0 #yritysten määrä
 
-    def __init__(self):
-        self.arvattu=[]
+
+    def __init__(self): #Luodaan uusi peli olio
+        self.oikein=[]
         self.sana=[]
-        self.vaara=[]
+        self.arvatut_kirjaimet=[]
         self.yritysta=0
-### määritetään metodi joka arpoo sanan 
-    def arvoSana(self, sana):
-        self.lista=[]
-        sana = sana.upper()
-        for char in sana:
-            self.lista.append(char)
-        self.arvattu=["_","_","_","_","_","_","_"]
+
+
+### määritetään metodi joka arpoo sanan ja tekee listan johon oikeat kirjaimet lisätään. 
+    def arvoSana(self):
+
+        with open("Sanoja.txt", "r") as file:  #Avataan tekstitiedosto johon on tallennettu sanoja. Arvotaan yksi sana.
+            sanoja = file.read()
+            sanoja = list(map(str, sanoja.split()))
+        uusi_sana = random.choice(sanoja)
+        uusi_sana = uusi_sana.upper() #Muutetaan kaikki merkit isoiksi. 
+
+        for char in uusi_sana:
+            self.sana.append(char) #Lisätään arvuuteltava sana "sana" listaan
+        for char in uusi_sana:
+            self.oikein.append("_") #tehdään tyhjä lista jossa on oikea määrä _ viivoja korvaamaan kirjaimet. 
+
 
 ###Määritetään metodi, joka tarkistaa onko arvattu kirjain sanassa.
     def tarkista(self,kirjain):
-        self.vaara.append(kirjain)
-        numero=0
-
-        if kirjain in self.lista:
-            for i in self.lista: 
-                if kirjain==self.lista[numero]:
-                    self.arvattu[numero]=i
-                    numero=numero+1
+        kirjain = kirjain.upper() #Muutetaan kirjain isoksi. 
+        self.arvatut_kirjaimet.append(kirjain) #Lisätään kirjain heti arvattujen listaan
+        x=0 #Apumuuttuja jonka avulla oikea kirjain saadaan oikeaan kohtaan listassa.
+        if kirjain in self.sana:
+            for i in self.sana: #käydään lista läpi merkki kerrallaan ja korvataan sanan "_" merkki kirjaimella.
+                if kirjain==self.sana[x]: 
+                    self.oikein[x]=i
+                    x=x+1
                 else:
-                    numero=numero+1
-        else:
-            self.vaara.append(kirjain)
-            self.yritysta=self.yritysta+1
+                    x=x+1
                 
+        else: #jos kirjain ei ole sanassa, lisätään 1 yrityksiin
+            self.yritysta=self.yritysta+1
 
+    def tulos(self):
+        if self.yritysta==9:
+            print("Hävisit pelin! Oikea sana oli: ","".join(uusipeli.sana))
 
+        if self.sana == self.oikein:
+            print("Onneksiolkoon voitit!")
 
-tikku=hirsipuu()
-
-kirjain="1"
-tikku.arvoSana("MAKKARA")
-while tikku.yritysta<9:
-    print(tikku.arvattu)
-    print(tikku.vaara)
-    print(tikku.yritysta)
-    kirjain=input("Anna kirjain")    
-    tikku.tarkista(kirjain)
-print("Hävisit")
+#Pääohjelma
+uusipeli=hirsipuu()
+uusipeli.arvoSana()
+while uusipeli.yritysta<9 and uusipeli.sana != uusipeli.oikein: #Kökkö tapa seurata kun peli voitetaan tai hävitään.
+    print("Arvattava sana\n"," ".join(uusipeli.oikein))
+    print("Arvatut kirjaimet\n",",".join(uusipeli.arvatut_kirjaimet))
+    print("Yrityksiä: ",uusipeli.yritysta,"/9")
+    kirjain=input("Anna kirjain: ")    
+    uusipeli.tarkista(kirjain)
+uusipeli.tulos() #Voiton/häviön jälkeen kutsutaan tulos metodia joka ilmoittaa tuloksen. 
